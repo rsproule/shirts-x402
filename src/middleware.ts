c4e83d914ef0563b5c4759e1ed637e93a6f182b4
Ryan Sproule <rsproule23@gmail.com>
2025-09-30 16:17:51 -0400

    prod 25$

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,14 +5,14 @@ export const middleware = paymentMiddleware(
   "0xc0541B06F703c6753B842D83cF62d55F93EE81bE", // rsproule merit wallet
   {
     "/api/shirts": {
-      price: "$0.01",
+      price: "$25.00",
       network: "base",
       config: {
         description: "AI-generated shirt design + purchase",
       },
     },
     "/api/shirts/from-image": {
-      price: "$0.01",
+      price: "$25.00",
       network: "base",
       config: {
         description: "Custom shirt from your image",
